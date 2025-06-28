9e6d01062087b52f8f9c9ccd0a379c3ad38a9dd2
Prithwis Dutta <prithwis1201@gmail.com>
2025-06-28 10:00:07 +0530

    quiz.py

---
+++ quiz.py
@@ -0,0 +1,447 @@
+# importing time to use the sleep fuction for more genuine and backend processing show.
+
+import time
+
+# calling the quiz
+
+def quiz():
+
+# login page name entry
+
+   print("What is Your name?")
+time.sleep(1)
+name = (input("Enter your name: "))
+
+if name == "Prithwis":
+  print("Hello", name)
+
+# starting the quiz
+
+  print("lets start the quiz")
+
+  time.sleep(2)
+
+# Quick initialization of the quiz (For aesthetic and genuine look)
+
+  print("Initializing the quiz.....................................................................")
+  time.sleep(5)
+
+  print("The quiz is ready to start")
+  time.sleep(2)
+  print("Starting the quiz in......................................................................")
+  time.sleep(1)
+  print("5....................................")
+  time.sleep(1)
+  print("4....................................")
+  time.sleep(1)
+  print("3....................................")
+  time.sleep(1)
+  print("2....................................")
+  time.sleep(1)
+  print("1....................................")
+  time.sleep(1)
+  print("Starting the quiz now, good luck !")
+  time.sleep(2)
+
+  # first question
+
+  print("What is the name of the Operating system that you are using ?")
+  print("a) Kali Linux")
+  print("b) Windows")
+  print("c) Ubuntu")
+  print("d) Fedora")
+
+  print("NOTE : Only enter the number of the option")
+
+  answer0 = input("Enter your answer: ")
+
+  if answer0 == "b":
+    print("You won, Correct answer !")
+    print(".......................Next Question coming up.......................")
+
+    time.sleep(2)
+
+    # second question
+
+    print("What is the coding language used to make this quiz ?")
+    print("a) C++")
+    print("b) CSS")
+    print("c) Python")
+    print("d) Javascript")
+
+    print("NOTE : Only enter the number of the option")
+
+    answer1 = input("Enter your answer: ")
+
+    if answer1 == "c":
+      
+      print("You won, Correct answer !")
+      print(".......................Next Question coming up.......................")
+
+      time.sleep(2)
+
+      # third question
+
+      print("What is the color of a dragonfruit ?")
+      print("a) red")
+      print("b) green")
+      print("c) orange")
+      print("d) blue")
+      print("NOTE : Only enter the number of the option")
+
+      answer2 = input("Enter your answer: ")
+
+      if answer2 == "a":
+        print("You won, Correct answer !")
+        print(".......................Next Question coming up.......................")
+
+        time.sleep(2)
+
+        # fourth question
+
+        print("Who is the creator of this quiz ?")
+        print("a) Jayjeet")
+        print("b) Ambarish")
+        print("c) Subendu")
+        print("d) Prithwis")
+        print("NOTE : Only enter the number of the option")
+
+        answer3 = input("Enter your answer: ")
+
+        if answer3 == "d":
+          print("You won, Correct answer !")
+          print(".......................Next Question coming up.......................")
+
+          time.sleep(2)
+
+          # fifth question
+
+          print("What is the capital of Australia ?")
+          print("a) Sydney")
+          print("b) Vienna")
+          print("c) Kolkata")
+          print("d) Austria")
+          print("NOTE : Only enter the number of the option")
+
+          answer4 = input("Enter your answer: ")
+
+          if answer4 == "a":
+            print("You won, Correct answer !")
+            print(".......................Next Question coming up.......................")
+
+            time.sleep(2)
+
+            # sixth question
+
+            print("Who is the president of Ukraine ?")
+            print("a) Donald Trump")
+            print("b) Zelensky")
+            print("c) Vladimir Putin")
+            print("d) Osama Bin Laden")
+            print("NOTE : Only enter the number of the option")
+
+            answer5 = input("Enter your answer: ")
+
+            if answer5 == "b":
+              print("You won, Correct answer !")
+              print(".......................Next Question coming up.......................")
+
+              time.sleep(2)
+
+              # seventh question
+
+              print("What is the name of the offline game of Microsoft Edge")
+              print("a) Chrome Dino")
+              print("b) Hitman")
+              print("c) Surf")
+              print("d) Subway Surfers")
+              print("NOTE : Only enter the number of the option")
+
+              answer6 = input("Enter your answer: ")
+
+              if answer6 == "c":
+                print("You won, Correct answer !")
+                print(".......................Next Question coming up.......................")
+
+                time.sleep(2)
+
+                # eighth question
+
+                print("What was the name of the bird used in the previous Twitter logo")
+                print("a) Larry")
+                print("b) Morson")
+                print("c) Lamar")
+                print("d) Lorose")
+                print("NOTE : Only enter the number of the option")
+
+                answer7 = input("Enter your answer: ")
+
+                if answer7 == "a":
+                  print("You won, Correct answer !")
+                  print(".......................Next Question coming up.......................")
+
+                  time.sleep(2)
+
+                  # ninth question
+
+                  print("How many keys are there in a keyboard ?")
+                  print("a) 86")
+                  print("b) 106")
+                  print("c) 107")
+                  print("d) 104")
+                  print("NOTE : Only enter the number of the option")
+
+                  answer8 = input("Enter your answer: ")
+
+                  if answer8 == "d":
+                    print("You won, Correct answer !")
+                    print(".......................Next Question coming up.......................")
+
+                    time.sleep(2)
+
+                    # tenth question
+
+                    print("What is the name of this laptop ?")
+                    print("a) DELL Inspiron")
+                    print("b) HP Pavilion")
+                    print("c) Lenovo Thinkpad")
+                    print("d) Lenovo Katana")
+                    print("NOTE : Only enter the number of the option")
+
+                    answer9 = input("Enter your answer: ")
+
+                    if answer9 == "c":
+                      print("You won, Correct answer !")
+                      print(".......................Next Question coming up.......................")
+
+                      time.sleep(2)
+
+                      # eleventh question
+
+                      print("What is the most beginner friendly programming language ?")
+                      print("a) C++")
+                      print("b) Python")
+                      print("c) Java")
+                      print("d) Ruby")
+
+                      answer10 = input("Enter your answer: ")
+
+                      if answer10 == "b" :
+                        print("You won, Correct answer !")
+                        print(".......................Next Question coming up.......................")
+
+                        time.sleep(2)
+
+                        # twelfth question
+
+                        print("Which spectrum of light has the highest frequency ?")
+                        print("a) Infrared")
+                        print("b) Ultraviolet")
+                        print("c) X-ray")
+                        print("d) Gamma-ray")
+                        print("NOTE : Only enter the number of the option")
+
+                        answer11 = input("Enter your answer: ")
+
+                        if answer11 == "d":
+                          print("You won, Correct answer !")
+                          print(".......................Next Question coming up.......................")
+
+                          time.sleep(2)
+
+                          # thirteenth question 
+
+                          print("What is the name of the first computer virus ?")
+                          print("a) ILOVEYOU")
+                          print("b) Creeper")
+                          print("c) Brain")
+                          print("d) Elk Cloner")
+                          print("NOTE : Only enter the number of the option")
+
+                          answer12 = input("Enter your answer: ")
+
+                          if answer12 == "b":
+                            print("You won, Correct answer !")
+                            print(".......................Next Question coming up.......................")
+
+                            time.sleep(2)
+
+                            # fourteenth question
+
+                            print("What is the name of the first computer ?")
+                            print("a) ENIAC")
+                            print("b) UNIVAC")
+                            print("c) Z3")
+                            print("d) ABC")
+                            print("NOTE : Only enter the number of the option")
+
+                            answer13 = input("Enter your answer: ")
+
+                            if answer13 == "a":
+                              print("You won, Correct answer !")
+                              print(".......................Next Question coming up.......................")
+
+                              time.sleep(2)
+
+                              # fifteenth question
+
+                              print("What is the name of the first computer programmer ?")
+                              print("a) Ada Lovelace")
+                              print("b) Grace Hopper")
+                              print("c) Alan Turing")
+                              print("d) Charles Babbage")
+                              print("NOTE : Only enter the number of the option")
+
+                              answer14 = input("Enter your answer: ")
+
+                              if answer14 == "a":
+                                print("You won, Correct answer !")
+                                print(".......................Next Question coming up.......................")
+
+                                time.sleep(2)
+
+                                # sixteenth question
+
+                                print("What is the name of the first computer language ?")
+                                print("a) COBOL")
+                                print("b) FORTRAN")
+                                print("c) C")
+                                print("d) BASIC")
+                                print("NOTE : Only enter the number of the option")
+
+                                answer15 = input("Enter your answer: ")
+
+                                if answer15 == "b":
+                                  print("You won, Correct answer !")
+                                  print(".......................Next Question coming up.......................")
+
+                                  time.sleep(2)
+
+                                  # seventeenth question
+
+                                  print("What is the name of the first computer ?")
+                                  print("a) ENIAC")
+                                  print("b) UNIVAC")
+                                  print("c) Z3") 
+                                  print("d) ABC")
+                                  print("NOTE : Only enter the number of the option")
+
+                                  answer16 = input("Enter your answer: ")
+
+                                  if answer16 == "a":
+                                    print("You won, Correct answer !")
+                                    print(".......................Next Question coming up.......................")
+
+                                    time.sleep(2)
+
+                                    # eighteenth question
+
+                                    print("When did Aurangzeb die ?")
+                                    print("a) 1607")
+                                    print("b) 1708")
+                                    print("c) 1707")
+                                    print("d) 1526")
+                                    print("NOTE : Only enter the number of the option")
+
+                                    answer17 = input("Enter your answer: ")
+
+                                    if answer17 == "c":
+                                      print("You won, Correct answer !")
+                                      print(".......................Next Question coming up.......................")
+
+# Exit commands for if the player has commited any mistake
+
+                                    else:
+                                      print("You lost wrong answer")
+                                      print("Get out")
+                                      exit()
+
+                                  else:
+                                    print("You lost wrong answer")
+                                    print("Get out")
+                                    exit()
+
+                                else:
+                                 print("You lost wrong answer")
+                                 print("Get out")
+                                 exit()
+
+                              else:
+                                print("You lost wrong answer")
+                                print("Get out")
+                                exit()
+
+                            else:
+                              print("You lost wrong answer")
+                              print("Get out")
+                              exit()  
+
+                          else:
+                            print("You lost wrong answer")
+                            print("Get out")
+                            exit()
+
+                        else:
+                          print("You lost wrong answer")
+                          print("Get out")
+                          exit()
+
+                      else:
+                        print("You lost wrong answer")
+                        print("Get out")
+                        exit()
+
+                    else:
+                      print("You lost wrong answer")
+                      print("Get out")
+                      exit()
+
+                    
+                  else:
+                    print("You lost wrong answer")
+                    print("Get out")
+                    exit()
+
+                else:
+                  print("You lost wrong answer")
+                  print("Get out")
+                  exit()
+
+              else:
+                print("You lost wrong answer")
+                print("Get out")
+                exit()
+
+            else:
+              print("You lost wrong answer")
+              print("Get out")
+              exit()
+
+          else:
+            print("You lost wrong answer")
+            print("Get out")
+            exit()
+
+        else:
+          print("You lost wrong answer")
+          print("Get out")
+          exit()
+
+      else:
+        print("You lost wrong answer")
+        print("Get out")
+        exit()
+      
+
+    else:
+      print("You lost, Wrong answer")
+      print("Get out")
+      exit()
+  
+    quiz()
+
+  else:
+    print("You are not the owner of this device")
+    print("You are not allowed to use this device")
+    print("Please get out")
+    exit()
